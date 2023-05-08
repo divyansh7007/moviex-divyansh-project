e8d72dca3b001d38dbb8acbe259a3b75f6ba4f34
divyansh7007 <119783951+divyansh7007@users.noreply.github.com>
2023-05-08 14:57:36 +0530

    Now, In Movix Header Footer and HeroBanner section are compeleted

--- src/App.jsx
+++ src/App.jsx
@@ -34,14 +34,14 @@ function App() {
   });
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/:mediaType/:id' element={<Details />} />
         <Route path='/search/:query' element={<SearchResult />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   )
 }
