5899024e8230e79d587333a8b410b95ac6deff10
divyansh7007 <119783951+divyansh7007@users.noreply.github.com>
2023-05-15 14:52:03 +0530

    Here, I compeleted the top cast and details components

--- src/pages/details/details.jsx
+++ src/pages/details/details.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+
 
 import './style.scss'
 
-import useFetch from '../../hooks/useFetch'
 import DetailsBanner from './detailsBanner/DetailsBanner'
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
+import Cast from './cast/Cast';
+import VideosSection from './videoSection/videoSection';
 
 const Details = () => {
 
-  // const { mediaType, id } = useParams();
-  // const { data, loading } = useFetch(`/${mediaType}/${id}`)
-  // console.log(data?.homepage);
-  // console.log(loading);
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`)
+  const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`)
 
   return (
     <div>
-      <DetailsBanner />
+      <DetailsBanner video={data?.results[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
     </div>
   )
 }
